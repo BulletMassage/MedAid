a16e337bf26a97374e97721f4744d515619ebe82
bulletmassage <cassidy.norfleet@yahoo.com>
2019-05-12 15:14:00 -0700

    Updated activities to fragments and added bottom drawer navigation.

--- app/src/main/java/com/example/medaid/MainActivity.java
+++ app/src/main/java/com/example/medaid/MainActivity.java
@@ -1,34 +1,55 @@
 package com.example.medaid;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    /*--------------Lifecycle Callbacks--------------*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        /* if profile was not created (first time launching app) start profile activity
-        if (name == NULL) {
-            Intent intent = new Intent(MainActivity.this, Profile.class);
-            startActivity(intent);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HomeFragment()).commit();
         }
-        */
     }
 
-    /*--------------My Functions--------------*/
-    public void onClickProfile(View view) {
-        Intent intent = new Intent(MainActivity.this, Profile.class);
-        startActivity(intent);
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
     }
 
-    public void onClickCabinet(View view) {
-        Intent intent = new Intent(MainActivity.this, Cabinet.class);
-        startActivity(intent);
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_cabinet:
+                            selectedFragment = new CabinetFragment();
+                            break;
+                        case R.id.nav_profile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
